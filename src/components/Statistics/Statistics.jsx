2e6b4edd7d66abccf171ce32f47850415bac0b59
MD AZHARUDDIN <azhar73397@gmail.com>
2024-04-27 10:51:12 +0600

    Extra two section added in home page

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -13,31 +13,29 @@ const Statistics = () => {
     </div>
     <div className="flex flex-col lg:flex-row justify-center gap-8 mt-14">
         <div className="text-center space-y-3">
-            <div className="radial-progress text-primary rotate-180" style={{ "--value": 87 }} role="progressbar"><span
-                className="rotate-180">87%</span></div>
-            <h3 className="text-blacked font-semibold">Prayer Facility</h3>
-            <p className="text-text-body">We have 87% prayer facility more <br /> than other club. 95%.</p>
+            <div className="radial-progress text-primary rotate-180" style={{ "--value": 15 }} role="progressbar"><span
+                className="rotate-180">15%</span></div>
+            <h3 className="text-blacked font-semibold">Child <br />
+            (Under 18 years old)</h3>
+            <p className="text-text-body">Common types of travel: <br /> Family vacations, school trips, and visiting relatives.</p>
         </div>
         <div className="text-center space-y-3">
-            <div className="radial-progress text-green-500 rotate-180" style={{ "--value": 95 }} role="progressbar"> <span
-                className="rotate-180">95%</span></div>
-            <h3 className="text-blacked font-semibold">Experienced Coach</h3>
-            <p className="text-text-body">Opportunities for coaches to network<br />
-                with other coaches.</p>
+            <div className="radial-progress text-green-500 rotate-180" style={{ "--value": 30 }} role="progressbar"> <span
+                className="rotate-180">30%</span></div>
+            <h3 className="text-blacked font-semibold">Young Adult <br /> (18-34 years old)</h3>
+            <p className="text-text-body">Common types of travel: Backpacking trips, adventure travel,  <br />  solo travel, and weekend getaways with friends.</p>
         </div>
         <div className="text-center space-y-3">
-            <div className="radial-progress text-blue-700 rotate-180" style={{ "--value": 90 }} role="progressbar"><span
-                className="rotate-180">90%</span></div>
-            <h3 className="text-blacked font-semibold">Senior Player</h3>
-            <p className="text-text-body">Senior hockey refers to amateur <br />
-                ice hockey competition.</p>
+            <div className="radial-progress text-blue-700 rotate-180" style={{ "--value": 40 }} role="progressbar"><span
+                className="rotate-180">40%</span></div>
+            <h3 className="text-blacked font-semibold">Adult <br />(35-64 years old)</h3>
+            <p className="text-text-body">Common types of travel: <br /> Family vacations, business travel, cultural trips,  and beach holidays.</p>
         </div>
         <div className="text-center space-y-3">
-            <div className="radial-progress text-accent rotate-180" style={{ "--value": 80 }} role="progressbar"><span
-                className="rotate-180">80%</span></div>
-            <h3 className="text-blacked font-semibold">Training Ground</h3>
-            <p className="text-text-body">You can start training<br />
-                to take your game.</p>
+            <div className="radial-progress text-accent rotate-180" style={{ "--value": 15 }} role="progressbar"><span
+                className="rotate-180">15%</span></div>
+            <h3 className="text-blacked font-semibold">Older Adult <br /> (65 years old and above)</h3>
+            <p className="text-text-body">Common types of travel:  <br />  Retirement travel, <br /> senior tours, cruises, and visiting grandchildren.</p>
         </div>
     </div>
 </section>
